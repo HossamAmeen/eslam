10abe8d114a9bf0a9d52b3055b0817ab5ed0ac2d
hossam <hossam@gmail.com>
2021-03-11 14:36:10 -0500

    edit from server

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     ]);
        factory('App\Models\Configration',1)->create();
     //    factory('App\Models\News',90)->create();
-       factory('App\Models\Article',90)->create();
-       factory('App\Models\Service',90)->create();
+      factory('App\Models\Article',90)->create();
+      factory('App\Models\Service',90)->create();
     //    factory('App\Models\Question',90)->create();
     //    factory('App\Models\Gallery',19)->create();
     //    factory('App\Models\Video',5)->create();
