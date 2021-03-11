0ebe0a2c3d8eb91ad585ebc7b990e879f90367a7
hossam ameen <hosamameen948@gmail.com>
2021-03-11 21:50:57 +0200

    edit index for blade

--- app/Http/Controllers/FrontEnd/HomeController.php
+++ app/Http/Controllers/FrontEnd/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\FrontEnd;
 
 use Illuminate\Http\Request;
 use App\Models\News;
--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Route::prefix('admin')->group(function(){
         Route::resource('videos', 'VideoController'); 
     });
 });
-Route::get('/', 'BackEnd/HomeController@index')->name('home');
+Route::get('/', 'FrontEnd/HomeController@index')->name('home');
 Route::get('article/{articleId}', 'HomeController@article')->name('article');
