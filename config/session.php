7ef32fe2dff8bb715a64071f78f63f6ac948aa47
hossam <hossamameen948@gmail.com>
2021-05-02 11:56:25 +0200

    edit session name

--- config/session.php
+++ config/session.php
@@ -11,7 +11,7 @@ return [
     |
     | This option controls the default session "driver" that will be used on
     | requests. By default, we will use the lightweight native driver but
-    | you may specify any of the other wonderful drivers provided here.
+    | you may specify any of the otfher wonderful drivers provided here.
     |
     | Supported: "file", "cookie", "database", "apc",
     |            "memcached", "redis", "dynamodb", "array"
@@ -126,7 +126,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug("elslam",'_').'_session'
     ),
 
     /*
